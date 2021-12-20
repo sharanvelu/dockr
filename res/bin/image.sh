e5144af17b8263675e0e41f71a140ef845d7e754
Sharan <sharan@mallow-tech.com>
2021-12-20 15:23:43 +0530

    - Updated image command. - Added image to dynamic command execution.

--- res/bin/image.sh
+++ res/bin/image.sh
@@ -2,29 +2,40 @@
 
 ## Dockr by Sharan
 
-# shellcheck disable=SC2089
-DOCKR_IMAGE_LIST_COMMAND="docker image ls \
+DOCKR_IMAGE_LIST_ALL="docker image ls \
         -f reference=sharanvelu/laravel-php \
         -f reference=ubuntu/mysql \
         -f reference=redis \
         -f reference=postgres"
 
-# List all images
-if [ "$1" == "ls" ]; then
-    ${DOCKR_IMAGE_LIST_COMMAND}
+DOCKR_IMAGE_LIST_ASSET="docker image ls \
+       -f reference=ubuntu/mysql \
+       -f reference=redis \
+       -f reference=postgres"
 
-elif [ "$1" == "prune" ]; then
+DOCKR_IMAGE_LIST_PROJECT="docker image ls \
+       -f reference=sharanvelu/laravel-php"
+
+# List all images
+if [ "$2" == "ls" ]; then
+    echo -e "${YELLOW}ASSET Images : ${CLR}"
+    ${DOCKR_IMAGE_LIST_ASSET}
+
+    echo ""
+    echo -e "${YELLOW}Project Images : ${CLR}"
+    ${DOCKR_IMAGE_LIST_PROJECT}
+
+elif [ "$2" == "prune" ]; then
     dockr down all -f >> /dev/null
 
-    if [ "$(${DOCKR_IMAGE_LIST_COMMAND} -q)" ]; then
+    if [ "$(${DOCKR_IMAGE_LIST_ALL} -q)" ]; then
         # shellcheck disable=SC2046
-        docker image rm $(${DOCKR_IMAGE_LIST_COMMAND} -q) >> /dev/null
+        docker image rm $(${DOCKR_IMAGE_LIST_ALL} -q) >> /dev/null
 
         echo -e "All ${DOCKR_NAME} images are ${GREEN}pruned successfully${CLR}."
     else
         echo -e "${RED}No${CLR} ${DOCKR_NAME} images to prune."
     fi
-
 
 else
     # Display help command
